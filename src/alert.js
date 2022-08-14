176126edc8fda3830e24ab933bffad62b24e9862
= <=>
2022-08-14 21:22:09 +0700

    balel

---
+++ src/alert.js
@@ -0,0 +1,4 @@
+const alert1 = () => {
+    alert('xin chao')
+}
+export default alert1;
---
+++ src/component/Button/index.js
@@ -0,0 +1,7 @@
+import alert1 from "~/alert";
+function Button(){
+    return (
+        <button onClick={alert1}>Click me!</button>
+    )
+}
+export default Button;
